dcb652dd0d5bb1e28ccc34a5e124176acf663a21
francozanini <francozanini1995@gmail.com>
2022-09-08 19:48:04 -0300

    simplified state, introduced TrackerCell, added symbols for each trackerState

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,7 @@ type TrackableName = 'humanity' | 'hunger' | 'health' | 'willpower';
 
 interface Trackable {
     name: TrackableName;
-    total: number;
-    superficial: number;
-    aggravated: number;
+    track: TrackerState[];
 }
 
 interface Kindred {
@@ -29,52 +27,75 @@ const dummyData: Kindred[] = [
     {
         name: 'Boris',
         trackable: [
-            { name: 'humanity', total: 6, superficial: 0, aggravated: 1 },
-            { name: 'hunger', total: 5, superficial: 0, aggravated: 0 },
-            { name: 'health', total: 5, superficial: 0, aggravated: 0 },
-            { name: 'willpower', total: 5, superficial: 0, aggravated: 0 }
+            { name: 'humanity', track: ['empty', 'empty', 'empty', 'empty', 'empty'] },
+            { name: 'hunger', track: ['empty', 'empty', 'empty', 'empty', 'empty'] },
+            { name: 'health', track: ['empty', 'empty', 'empty', 'empty', 'empty'] },
+            { name: 'willpower', track: ['empty', 'empty', 'empty', 'empty', 'empty'] }
         ]
     }
 ];
 
-const arrayFromTrackable = (trackable: Trackable): Point[] => {
-    console.log(trackable);
-    const from = Array.from(
-        { length: trackable.total },
-        (_, i) =>
-            ({
-                index: i,
-                state: i < trackable.aggravated ? 'aggravated' : 'empty'
-            } as Point)
-    );
-    console.log(from);
-    return from;
+const nextTrackerState = (state: TrackerState) => {
+    const stateMappings = new Map<TrackerState, TrackerState>([
+        ['empty', 'superficial'],
+        ['superficial', 'aggravated'],
+        ['aggravated', 'empty']
+    ]);
+
+    return stateMappings.get(state);
 };
 
-function Tracker({ trackable, changeKindred }) {
-    const points = arrayFromTrackable(trackable);
+function TrackerCell({
+    state,
+    changeKindred,
+    i
+}: {
+    state: TrackerState;
+    changeKindred: (newState: TrackerState, i: number) => void;
+    i: number;
+}) {
+    const symbol = state === 'aggravated' ? 'X' : state === 'superficial' ? '/' : '';
+
+    return (
+        <div
+            style={{
+                margin: '1px',
+                border: '1px solid black',
+                width: '20px',
+                height: '20px',
+                fontSize: '18px',
+                textAlign: 'center'
+            }}
+            onClick={() => changeKindred(nextTrackerState(state), i)}>
+            {symbol}
+        </div>
+    );
+}
+
+function Tracker({
+    trackable,
+    changeTrackable
+}: {
+    trackable: Trackable;
+    changeTrackable: (trackable: Trackable) => void;
+}) {
+    const changeTracker = (newState: TrackerState, i: number) =>
+        changeTrackable({
+            name: trackable.name,
+            track: trackable.track.map((state, j) => (j === i ? newState : state))
+        });
 
     return (
         <div style={{ display: 'flex' }}>
             <h3>{trackable.name}</h3>
-            {points.map((point) => {
-                const backgroundColor = point.state === 'aggravated' ? 'pink' : 'white';
+            {trackable.track.map((track, index) => {
                 return (
-                    <div
-                        key={point.index}
-                        style={{
-                            backgroundColor: backgroundColor,
-                            margin: '1px',
-                            border: '1px solid black',
-                            width: '20px',
-                            height: '20px'
-                        }}
-                        onClick={() => {
-                            changeKindred(
-                                trackable.name,
-                                point.state === 'empty' ? trackable.aggravated + 1 : trackable - 1
-                            );
-                        }}></div>
+                    <TrackerCell
+                        key={index}
+                        i={index}
+                        state={track}
+                        changeKindred={changeTracker}
+                    />
                 );
             })}
         </div>
@@ -84,12 +105,18 @@ function Tracker({ trackable, changeKindred }) {
 const IndexPage = () => {
     const [kindred, setKindred] = useState(dummyData);
 
-    const changeKindred = (kindredName: string, trackable: Trackable[]) => {
+    const changeKindred = (kindredName: string, changedTrackable: Trackable) => {
         const newKindred = kindred.map((kin) =>
-            kin.name == kindredName ? ({ name: kindredName, trackable } as Kindred) : kin
+            kin.name == kindredName
+                ? ({
+                    name: kindredName,
+                    trackable: kin.trackable.map((trackable) =>
+                        trackable.name === changedTrackable.name ? changedTrackable : trackable
+                    )
+                } as Kindred)
+                : kin
         );
         setKindred(newKindred);
-        console.log(kindred);
     };
 
     return (
@@ -101,13 +128,8 @@ const IndexPage = () => {
                         <Tracker
                             key={t.name}
                             trackable={t}
-                            changeKindred={(trackableName: TrackableName, aggravated: number) =>
-                                changeKindred(
-                                    name,
-                                    trackable.map((t2) =>
-                                        t2.name === trackableName ? { ...t2, aggravated } : t2
-                                    )
-                                )
+                            changeTrackable={(changedTrackable: Trackable) =>
+                                changeKindred(name, changedTrackable)
                             }
                         />
                     ))}
