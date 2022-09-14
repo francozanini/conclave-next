3ce01bc713a0e85a8cd9867cfefc878149e754cf
francozanini <francozanini1995@gmail.com>
2022-09-14 20:13:39 -0300

    added sire

--- pages/kindred/[id].tsx
+++ pages/kindred/[id].tsx
@@ -9,14 +9,13 @@ import TextInput from "../../components/TextInput";
 
 const KindredSheetPage = () => {
   const {id: kindredId} = useRouter().query;
-  const {register, handleSubmit} = useForm();
+  const {register, handleSubmit, watch} = useForm({mode: "onSubmit"});
 
   const {
     isLoading,
     isError,
     data: kindred,
   } = trpc.useQuery(["find-kindred", {kindredId: +kindredId}]);
-
   const mutation = trpc.useMutation(["update-kindred-details"]);
 
   if (isLoading) {
@@ -41,8 +40,15 @@ const KindredSheetPage = () => {
             label="ambition"
           />
           <TextInput defaultValue={kindred.desire} input={{...register("desire")}} label="desire" />
+          <TextInput defaultValue={kindred.sire} input={{...register("sire")}} label="sire" />
+          <select
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            id="countries">
+            <option selected>Choose a clan</option>
+            <option value="BRUJAH">Brujah</option>
+          </select>
           <button
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="mt-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             type="submit">
             Submit
           </button>
