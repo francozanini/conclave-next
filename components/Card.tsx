a12026df21eab82a0cb5e77729c2616267d9a565
francozanini <francozanini1995@gmail.com>
2022-09-14 19:17:32 -0300

    accept one child component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,4 @@
-const Card = ({children}: {children: JSX.Element[]}) => {
+const Card = ({children}: {children: JSX.Element[] | JSX.Element}) => {
   return <div className={"bg-gray-800 shadow-md p-6 rounded-lg"}>{children}</div>;
 };
 
