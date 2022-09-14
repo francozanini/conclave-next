4ae5ce1ae878437ce211565007de6a354ec6a993
francozanini <francozanini1995@gmail.com>
2022-09-13 21:52:28 -0300

    added clans

--- scripts/seeder.ts
+++ scripts/seeder.ts
@@ -1,18 +1,26 @@
+import {ClanName} from "@prisma/client";
+
 import {prisma} from "../backend/utils/prisma";
 
 const initialData = [
   {
     name: "Boris Dragunov",
+    ambition: "",
+    desire: "",
     experience: 3,
-    strength: 0,
-    dexterity: 0,
-    stamina: 0,
-    charisma: 0,
-    manipulation: 0,
-    composure: 0,
-    intelligence: 0,
-    wits: 0,
-    resolution: 0,
+    strength: 4,
+    dexterity: 3,
+    stamina: 2,
+    charisma: 3,
+    manipulation: 1,
+    composure: 3,
+    intelligence: 2,
+    wits: 2,
+    resolve: 2,
+
+    clan: {
+      name: ClanName.BRUJAH,
+    },
 
     aggravatedHealth: 0,
     superficialHealth: 0,
@@ -20,7 +28,7 @@ const initialData = [
     aggravatedWillpower: 0,
     superficialWillpower: 0,
 
-    humanity: 0,
+    humanity: 6,
     damagedHumanity: 0,
 
     hunger: 0,
@@ -28,7 +36,19 @@ const initialData = [
 ];
 
 const seedKindred = async () => {
-  await prisma.chronicle.create({data: {name: "test", kindred: {create: initialData}}});
+  await prisma.chronicle.create({
+    data: {
+      name: "test",
+      kindred: {
+        create: initialData.map((kin) => ({
+          ...kin,
+          clan: {
+            create: kin.clan,
+          },
+        })),
+      },
+    },
+  });
 };
 
 seedKindred();
