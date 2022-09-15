159d65870950e9523b5f34c730d4da21f9d4ed47
francozanini <francozanini1995@gmail.com>
2022-09-15 19:40:04 -0300

    change clan mutation client side

--- pages/kindred/[id].tsx
+++ pages/kindred/[id].tsx
@@ -1,3 +1,4 @@
+import { ClanName } from "@prisma/client";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 import Card from "../../components/Card";
@@ -14,7 +15,9 @@ const KindredSheetPage = () => {
     isError,
     data: kindred,
   } = trpc.useQuery(["find-kindred", {kindredId: +kindredId}]);
-  const mutation = trpc.useMutation(["update-kindred-details"]);
+  const detailsMutation = trpc.useMutation(["update-kindred-details"]);
+  const clanMutation = trpc.useMutation(["pick-clan"]);
+  const clans = Object.values(ClanName);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -25,36 +28,41 @@ const KindredSheetPage = () => {
   }
 
   const handleChange = debounce((e) => {
-    mutation.mutate({...getValues(), kindredId: +kindredId} as any);
+    detailsMutation.mutate({...getValues(), kindredId: +kindredId} as any);
   }, 300);
 
   return (
     <section className="mx-2 mt-2">
       <Card>
-        <form>
-          <TextInput defaultValue={kindred.name} input={{...register("name")}} label="name" />
-          <TextInput
-            defaultValue={kindred.ambition}
-            input={{...register("ambition")}}
-            label="ambition"
-          />
-          <TextInput
-            defaultValue={kindred.desire}
-            input={{...register("desire"), onChange: handleChange}}
-            label="desire"
-          />
-          <TextInput
-            defaultValue={kindred.sire}
-            input={{...register("sire"), onChange: handleChange}}
-            label="sire"
-          />
-          <select
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            id="countries">
-            <option>Choose a clan</option>
-            <option value="BRUJAH">Brujah</option>
-          </select>
-        </form>
+        <TextInput defaultValue={kindred.name} input={{...register("name")}} label="name" />
+        <TextInput
+          defaultValue={kindred.ambition}
+          input={{...register("ambition")}}
+          label="ambition"
+        />
+        <TextInput
+          defaultValue={kindred.desire}
+          input={{...register("desire"), onChange: handleChange}}
+          label="desire"
+        />
+        <TextInput
+          defaultValue={kindred.sire}
+          input={{...register("sire"), onChange: handleChange}}
+          label="sire"
+        />
+        <select
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          defaultValue={kindred.clan.name}
+          name="clans"
+          onChange={(e) =>
+            clanMutation.mutate({chosenClan: e.target.value as ClanName, kindredId: +kindredId})
+          }>
+          {clans.map((clan) => (
+            <option key={clan} value={clan}>
+              {clan}
+            </option>
+          ))}
+        </select>
       </Card>
     </section>
   );
