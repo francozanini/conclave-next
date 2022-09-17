1ec0cdf5b2023ef827c54799ebbb3b8ea40fa129
francozanini <francozanini1995@gmail.com>
2022-09-17 08:20:58 -0300

    remove unused

--- interfaces/Trackable.tsx
+++
@@ -1,7 +0,0 @@
-import { TrackerState } from './TrackerState';
-import { TrackableName } from '../types/TrackableName';
-
-export interface Trackable {
-    name: TrackableName;
-    track: TrackerState[];
-}
