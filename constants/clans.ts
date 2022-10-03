2c508bdaff954b0a2697e133a4a67917d00e2272
francozanini <francozanini1995@gmail.com>
2022-10-03 20:30:49 -0300

    move clans and disciplines to constnats folder

---
+++ constants/clans.ts
@@ -0,0 +1,21 @@
+import {ClanName} from "@prisma/client";
+
+const clans: {name: ClanName}[] = [
+  {name: ClanName.BANU_HAQIM},
+  {name: ClanName.BRUJAH},
+  {name: ClanName.CAITIFF},
+  {name: ClanName.GANGREL},
+  {name: ClanName.HECATA},
+  {name: ClanName.LASOMBRA},
+  {name: ClanName.MALKAVIAN},
+  {name: ClanName.NOSFERATU},
+  {name: ClanName.RAVNOS},
+  {name: ClanName.SALUBRI},
+  {name: ClanName.THE_MINISTRY},
+  {name: ClanName.TOREADOR},
+  {name: ClanName.TREMERE},
+  {name: ClanName.TZIMISCE},
+  {name: ClanName.VENTRUE},
+];
+
+export default clans;
