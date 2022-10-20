5ec46702e27e13eaaeb0d2e0b3051d3f003cdb87
francozanini <francozanini1995@gmail.com>
2022-10-19 23:55:22 -0300

    format power.cost, move closeButton into components/core

--- components/sheet/Powers.tsx
+++ components/sheet/Powers.tsx
@@ -7,6 +7,7 @@ import {PowerWithDiscipline} from "../../types/PowerWithDiscipline";
 import {uniqueBy} from "../../utils/uniques";
 import {trpc} from "../../utils/trpc";
 import {FullDiscipline} from "../../types/FullDiscipline";
+import {CloseButton} from "../core/CloseButton";
 
 interface PowersProps {
   powers: PowerWithDiscipline[];
@@ -32,7 +33,7 @@ const PowersTableRow = ({power, index}: PowersTableRowProps) => {
         </th>
         <td className="py-4 px-6">{power.level}</td>
         <td className="py-4 px-6">{removeUnderscoreAndCapitalize(power.name)}</td>
-        <td className="py-4 px-6">{power.cost}</td>
+        <td className="py-4 px-6">{removeUnderscoreAndCapitalize(power.cost)}</td>
         <td className="py-4 px-6">
           {formatPoolResources(power.firstPoolResource, power.secondPoolResource)}
         </td>
@@ -50,29 +51,6 @@ const DialogTrigger = DialogPrimitive.Trigger;
 const DialogContent = DialogPrimitive.Content;
 const DialogClose = DialogPrimitive.Close;
 const StyledOverlay = DialogPrimitive.Overlay;
-
-function CloseButton() {
-  return (
-    <button
-      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-      data-modal-toggle="defaultModal"
-      type="button">
-      <svg
-        aria-hidden="true"
-        className="w-5 h-5"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg">
-        <path
-          clipRule="evenodd"
-          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-          fillRule="evenodd"
-        />
-      </svg>
-      <span className="sr-only">Close modal</span>
-    </button>
-  );
-}
 
 const LearnPowerButton = ({
   powers,
