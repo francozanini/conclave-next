e25b7e824a50c703da371894718904bbae005908
franco.zanini <francozanini1995@gmail.com>
2022-11-25 01:50:49 -0300

    linting

--- src/modules/core/Button.tsx
+++ src/modules/core/Button.tsx
@@ -14,6 +14,7 @@ const blueButtonStyles =
   'text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800';
 const grayButtonStyles =
   'border border-gray-200 bg-gray-200 text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700';
+
 const Button = ({
   children,
   className,
