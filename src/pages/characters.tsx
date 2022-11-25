bb366fe0fd86855724e915b64f0151cfe251ad73
franco.zanini <francozanini1995@gmail.com>
2022-11-24 23:04:08 -0300

    character card added, navigate to sheet when creating new character

--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -1,22 +1,36 @@
+import {useRouter} from 'next/router';
+
 import withSessionGuard from '../modules/auth/SessionGuard';
 import Button from '../modules/core/Button';
+import Card from '../modules/core/Card';
 import {trpc} from '../utils/trpcClient';
 
 function CharactersPage() {
+  const router = useRouter();
   const {data} = trpc.characters.findForUser.useQuery();
-  const {mutate} = trpc.characters.newCharacter.useMutation();
+  const {mutate} = trpc.characters.newCharacter.useMutation({
+    onSettled: createdKindred => router.push(`/kindred/${createdKindred?.id}`)
+  });
 
   return (
     <>
       <div className="m-2 justify-between flex">
-        <h1 className="text-4xl">My Characters</h1>
-        <Button color="gray" onClick={() => mutate()}>
-          Create Character
+        <h1 className="text-4xl font-bold uppercase">My Characters</h1>
+        <Button
+          className="font-bold px-4 py-3 uppercase"
+          color="gray"
+          onClick={() => mutate()}>
+          Create a Character
         </Button>
       </div>
-      <div>
+      <div className="justify-between mt-4 gap-4 items-center flex flex-col">
         {data?.map(kindred => (
-          <div key={kindred.id}>{kindred.name}</div>
+          <Card key={kindred.id} className="flex justify-between">
+            <div>{kindred.name}</div>
+            <Button borderless className="uppercase font-bold">
+              edit
+            </Button>
+          </Card>
         ))}
       </div>
     </>
