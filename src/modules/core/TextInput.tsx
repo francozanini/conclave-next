f54bbeec18ff35228382053ed35efe121e3b0086
franco.zanini <francozanini1995@gmail.com>
2022-11-25 02:00:17 -0300

    linting tailwind classnames

--- src/modules/core/TextInput.tsx
+++ src/modules/core/TextInput.tsx
@@ -1,5 +1,6 @@
-import {FieldValues, UseFormRegister} from "react-hook-form";
-import {useState} from "react";
+/* eslint-disable tailwindcss/migration-from-tailwind-2 */
+import {FieldValues, UseFormRegister} from 'react-hook-form';
+import {useState} from 'react';
 
 interface Props {
   label: string;
@@ -7,12 +8,12 @@ interface Props {
   input: React.InputHTMLAttributes<HTMLInputElement>;
 }
 
-const TextInput = ({label, defaultValue = "", input}: Props) => {
+const TextInput = ({label, defaultValue = '', input}: Props) => {
   return (
-    <div className="relative z-0 mb-6 w-full group">
+    <div className="group relative z-0 mb-6 w-full">
       <input
         {...input}
-        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        className="peer block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600"
         defaultValue={defaultValue}
         type="text"
       />
