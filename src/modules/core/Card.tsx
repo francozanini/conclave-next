d443da55e6f5ce5d41cbc119cb9cbc062b196272
franco.zanini <francozanini1995@gmail.com>
2023-01-29 11:03:04 +0100

    (style) fixed max-w tailwind issue

--- src/modules/core/Card.tsx
+++ src/modules/core/Card.tsx
@@ -4,11 +4,25 @@ interface CardProps {
   className?: string;
 }
 
-const Card = ({children, maxWidth = 'md', className = ''}: CardProps) => (
-  <div
-    className={`max-w-${maxWidth} w-full rounded-lg bg-gray-800 p-6 shadow-md ${className}`}>
-    {children}
-  </div>
-);
+const maxWidthClasses = {
+  md: 'max-w-md',
+  lg: 'max-w-lg',
+  xl: 'max-w-xl',
+  '2xl': 'max-w-2xl',
+  '4xl': 'max-w-4xl',
+  '8xl': 'max-w-8xl',
+  fit: 'max-w-fit'
+};
+
+const Card = ({children, maxWidth = 'md', className = ''}: CardProps) => {
+  const _maxWidth = maxWidthClasses[maxWidth];
+
+  return (
+    <div
+      className={`${_maxWidth} w-full rounded-lg bg-gray-800 p-6 shadow-md ${className}`}>
+      {children}
+    </div>
+  );
+};
 
 export default Card;
